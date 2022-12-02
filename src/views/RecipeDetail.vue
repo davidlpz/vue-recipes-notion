7bf74f060bc67d4155c81e00f0d7eb2bb0f861b6
David López <davidlpzgomez@gmail.com>
2022-12-02 12:30:05 +0100

    Add cover image and title to the RecipeDetail component

--- src/views/RecipeDetail.vue
+++ src/views/RecipeDetail.vue
@@ -1,7 +1,46 @@
 <template>
-  <main>
-    <div class="">This is a recipe.</div>
+  <main class="recipe-detail">
+    <div class="image-wrapper">      
+      <img class="recipe-image" :src="recipe.image" :alt="recipe.name" />
+    </div>
+    <div class="recipe-body">
+      <h1 class="recipe-title">{{ recipe.name }}</h1>
+    </div>
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, reactive } from 'vue';
+import { useRoute } from 'vue-router';
+import RecipesAPI from '@/api/RecipesApi.js';
+
+const route = useRoute();
+const recipes = ref([]);
+let recipe = ref({});
+
+
+const loadData = async () => {
+  try {
+    recipes.value = await new RecipesAPI().listRecipes();
+    recipe.value = recipes.value.find(item => item.id === route.params.id);
+    const response = await new RecipesAPI().loadRecipeBody(route.params.id);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+loadData();
+</script>
+
+<style scoped>
+.image-wrapper {
+  width: 100%;
+  height: 375px;
+}
+.recipe-image {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
